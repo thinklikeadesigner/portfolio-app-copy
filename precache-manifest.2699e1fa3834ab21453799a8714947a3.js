26455bf4d7bd67d629e873132ba22fde88d9d389
rebecca burch <rebecca.burch.stu@gmail.com>
2020-10-12 17:29:16 -0500

    Updates

---
+++ precache-manifest.2699e1fa3834ab21453799a8714947a3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d6e417927ad6059cc8bcfe7dd29984",
+    "url": "/portfolio-app-copy/index.html"
+  },
+  {
+    "revision": "da44b219123265e484f5",
+    "url": "/portfolio-app-copy/static/css/main.a61e0307.chunk.css"
+  },
+  {
+    "revision": "e44a3f5699b605d72d07",
+    "url": "/portfolio-app-copy/static/js/2.e48171dd.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio-app-copy/static/js/2.e48171dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da44b219123265e484f5",
+    "url": "/portfolio-app-copy/static/js/main.2ed4f215.chunk.js"
+  },
+  {
+    "revision": "80a4390b1b4ffc3387c4",
+    "url": "/portfolio-app-copy/static/js/runtime-main.cde07579.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/portfolio-app-copy/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio-app-copy/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio-app-copy/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio-app-copy/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/portfolio-app-copy/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio-app-copy/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
